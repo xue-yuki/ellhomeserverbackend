1f249d960fab845f2f399f7540da8186d283be28
xue-yuki <spinixff@gmail.com>
2026-05-27 15:58:14 +0700

    fix: allow all CORS origins to fix websocket 403

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
